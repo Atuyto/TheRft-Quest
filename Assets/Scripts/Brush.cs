4f4c19ec47217f1761692499ff5ba98c61fb29e3
Emeline <66369599+Emeline1710@users.noreply.github.com>
2024-10-24 10:35:48 +0200

    Ajout de la palette de couleurs

--- Assets/Scripts/Brush.cs
+++ Assets/Scripts/Brush.cs
@@ -4,7 +4,11 @@ public class BrushTrigger : MonoBehaviour
 {
     public Material redMaterial; 
     public Material blueMaterial; 
-    public Material greenMaterial; 
+    public Material greenMaterial;
+    public Material orangeMaterial;
+    public Material yellowMaterial;
+    public Material purpleMaterial;
+    public Material pinkMaterial;
 
     private Renderer targetRenderer;
     private Material originalMaterial;
@@ -41,6 +45,34 @@ public class BrushTrigger : MonoBehaviour
                 if (greenMaterial != null)
                 {
                     targetRenderer.material = greenMaterial;
+                }
+            }
+            else if (other.CompareTag("OrangeColor"))
+            {
+                if (orangeMaterial != null)
+                {
+                    targetRenderer.material = orangeMaterial;
+                }
+            }
+            else if (other.CompareTag("YellowColor"))
+            {
+                if (yellowMaterial != null)
+                {
+                    targetRenderer.material = yellowMaterial;
+                }
+            }
+            else if (other.CompareTag("PurpleColor"))
+            {
+                if (purpleMaterial != null)
+                {
+                    targetRenderer.material = purpleMaterial;
+                }
+            }
+            else if (other.CompareTag("PinkColor"))
+            {
+                if (pinkMaterial != null)
+                {
+                    targetRenderer.material = pinkMaterial;
                 }
             }
             else if (other.CompareTag("Canva"))
