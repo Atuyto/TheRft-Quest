a5db073d12b47ed79a524ea7b1d8b83b150bba7d
Emeline <66369599+Emeline1710@users.noreply.github.com>
2024-12-02 11:51:30 +0100

    Ajout du plane messages et suivi de la caméra

---
+++ Assets/Scripts/Model/FollowPlayer.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public OVRCameraRig mainCameraRig;
+    public Vector3 positionOffset = new Vector3(0, 0, 5);
+    public Vector3 rotationOffset = new Vector3(0, 0, 0);
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        if (mainCameraRig == null)
+        {
+            mainCameraRig = FindObjectOfType<OVRCameraRig>();
+        }
+
+        if (mainCameraRig != null)
+        {
+            mainCamera = mainCameraRig.centerEyeAnchor.GetComponent<Camera>();
+        }
+    }
+
+    void Update()
+    {
+        if (mainCamera != null)
+        {
+            Transform centerEyeCamera = mainCameraRig.centerEyeAnchor;
+
+            if (centerEyeCamera != null)
+            {
+                Vector3 targetPosition = centerEyeCamera.position;
+                targetPosition.x = mainCameraRig.transform.position.x + positionOffset.x;
+                targetPosition.z = mainCameraRig.transform.position.z + positionOffset.z + mainCameraRig.centerEyeAnchor.position.z;
+
+                transform.position = targetPosition;
+
+                transform.rotation = Quaternion.Euler(rotationOffset);
+            }
+        }
+    }
+}
