2bd170a79b7e5f5f65408fe5852ff672af7fa228
Emeline <66369599+Emeline1710@users.noreply.github.com>
2024-12-03 20:10:55 +0100

    Ajout du clavier dans le menu

--- Assets/Radial_Selection.cs
+++ Assets/Radial_Selection.cs
@@ -8,15 +8,16 @@ public class Radial_Selection : MonoBehaviour
 {
     public OVRInput.Button handType;
 
-    [Range(2,10)]
+    [Range(2, 10)]
     public int numberOfRadialPart;
 
     private int numberOfIcons = 3;
     public GameObject radialPartPrefab;
     public Transform radialPartCanvas;
-    public float angleBetweenPart=10;
+    public float angleBetweenPart = 10;
     public Transform handTransform;
     public Transform handTransform2;
+    public GameObject keyboard;
 
     public Canvas[] icons;
 
@@ -27,7 +28,6 @@ public class Radial_Selection : MonoBehaviour
     private List<GameObject> spawnedParts = new List<GameObject>();
     private int currentSelectedRadialPart = -1;
 
-    // Start is called before the first frame update
     void Start()
     {
         foreach (var canvas in canvases)
@@ -39,20 +39,21 @@ public class Radial_Selection : MonoBehaviour
         {
             icon.gameObject.SetActive(false);
         }
+
+        keyboard.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(OVRInput.GetDown(handType))
+        if (OVRInput.GetDown(handType))
         {
             SpawnRadialPart();
         }
-        if(OVRInput.Get(handType))
+        if (OVRInput.Get(handType))
         {
             GetSelectedRadialPart();
         }
-        if(OVRInput.GetUp(handType))
+        if (OVRInput.GetUp(handType))
         {
             HideAndTriggerSelected();
         }
@@ -73,10 +74,17 @@ public class Radial_Selection : MonoBehaviour
             canvas.gameObject.SetActive(false);
         }
 
-        if( currentSelectedRadialPart != 2)
+        keyboard.SetActive(false);
+
+        if (currentSelectedRadialPart != 2)
         {
             GameObject canvasToShow = canvases[currentSelectedRadialPart];
             canvasToShow.gameObject.SetActive(true);
+
+            if (currentSelectedRadialPart == 1)
+            {
+                keyboard.SetActive(true);
+            }
         }
         else
         {
@@ -84,6 +92,8 @@ public class Radial_Selection : MonoBehaviour
             {
                 canvas.gameObject.SetActive(false);
             }
+
+            keyboard.SetActive(false);
         }
     }
 
@@ -94,18 +104,18 @@ public class Radial_Selection : MonoBehaviour
 
         float angle = Vector3.SignedAngle(radialPartCanvas.up, centerToHandProjected, -radialPartCanvas.forward);
 
-        if(angle < 0)
+        if (angle < 0)
         {
             angle += 360;
         }
 
         Debug.Log("ANGLE " + angle);
 
-        currentSelectedRadialPart = (int) angle * numberOfRadialPart / 360;
+        currentSelectedRadialPart = (int)angle * numberOfRadialPart / 360;
 
         for (int i = 0; i < spawnedParts.Count; i++)
         {
-            if(i == currentSelectedRadialPart)
+            if (i == currentSelectedRadialPart)
             {
                 spawnedParts[i].GetComponent<Image>().color = Color.yellow;
                 spawnedParts[i].transform.localScale = 1.1f * Vector3.one;
@@ -135,7 +145,7 @@ public class Radial_Selection : MonoBehaviour
         radialPartCanvas.position = handTransform.position;
         radialPartCanvas.rotation = handTransform.rotation;
 
-        for (int i = 0; i<numberOfIcons; i++)
+        for (int i = 0; i < numberOfIcons; i++)
         {
             Canvas canvasToShow = icons[i];
             canvasToShow.transform.position = handTransform.position;
@@ -145,12 +155,12 @@ public class Radial_Selection : MonoBehaviour
         {
             Destroy(item);
         }
-        
+
         spawnedParts.Clear();
 
         for (int i = 0; i < numberOfRadialPart; i++)
         {
-            float angle = - i * 360 / numberOfRadialPart - angleBetweenPart/2;
+            float angle = -i * 360 / numberOfRadialPart - angleBetweenPart / 2;
             Vector3 radialPartEulerAngle = new Vector3(0, 0, angle);
 
             GameObject spawnedRadialPart = Instantiate(radialPartPrefab, radialPartCanvas);
